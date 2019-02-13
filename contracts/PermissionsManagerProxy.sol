16800996c68e6688d47296457d188551c6ec84d7
Terence Yak <yakkiansheng@gmail.com>
2019-02-13 23:32:33 +0800

    Deleted proxy contracts for revamp

--- contracts/PermissionsManagerProxy.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.4.25;
-
-import "./Proxy.sol";
-
-contract PermissionsManagerProxy is Proxy {
-    constructor(address _initialImplementation) public Proxy(_initialImplementation) {}
-}
--- contracts/PermissionsTestProxy.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.4.25;
-
-import "./Proxy.sol";
-
-contract PermissionsTestProxy is Proxy {
-    constructor(address _initialImplementation) public Proxy(_initialImplementation) {}
-}
